a69db8309d85141b807a6d4bfac80b0b60eda489
petrus226 <lluisxl@gmail.com>
2018-06-17 13:25:58 +0200

    Initial contribution

---
+++ spec/sinatra_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../src/basics'
+
+require "test/unit"
+require 'rspec'
+require 'rack/test'
+
+describe 'The HelloWorld App' do
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  it "says hello" do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq('Hello, World')
+  end
+end
